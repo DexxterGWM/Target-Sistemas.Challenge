1c1d7b781e77fa68dc977d7e0cb71a669d8435f5
Dexxter <99261879+DexxterGWM@users.noreply.github.com>
2023-04-11 13:18:56 -0300

    main.js File

---
+++ main.js
@@ -0,0 +1,11 @@
+import sumResult from './Scripts/sumResult.js';
+import isFibonacci from './Scripts/isFibonacci.js';
+import billStatus from './Scripts/billStatus.js';
+import stateShare from './Scripts/stateShare.js';
+import reverseWord from './Scripts/reverseWord.js';
+
+sumResult();
+isFibonacci();
+billStatus();
+stateShare();
+reverseWord();
